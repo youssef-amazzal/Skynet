fcf3754e12bc3be2c2333f6c4033176d35ed6d12
imane_quabbou <103893606+imane12j@users.noreply.github.com>
2022-06-12 20:54:25 +0100

    Add navigation bar

--- FlightBookingApplication/src/main/java/Main.java
+++ FlightBookingApplication/src/main/java/Main.java
@@ -1,7 +1,30 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import view.NavigationBar;
 
-public class Main {
+public class Main extends Application {
 	
 	public static void main(String[] args) {
-		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			HBox root = new HBox();
+			root.getStylesheets().add(getClass().getResource("/style/Application.css").toExternalForm());
+			Scene scene = new Scene(root, 1366, 768);
+			
+			root.getChildren().add(NavigationBar.getNavigationBar());
+			
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Flight Booking Application");
+			primaryStage.show();			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
