bfd564ad5e68300a5b098eebca14a4e2dfacaba2
imane_quabbou <103893606+imane12j@users.noreply.github.com>
2022-06-16 16:43:53 +0100

    Add Sign-in and Sign-up interfaces

--- FlightBookingApplication/src/main/java/application/Main.java
+++ FlightBookingApplication/src/main/java/application/Main.java
@@ -6,10 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import view.NavigationBar;
 import view.Palette;
 import java.util.Objects;
+import javafx.scene.Parent;
 
 public class Main extends Application {
 	
@@ -20,40 +23,49 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			HBox root = new HBox();
+			
+          /*HBox root = new HBox();
+			
 			root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style/Application.css")).toExternalForm());
 			Scene scene = new Scene(root, 1366, 768);
+			StackPane  page = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Sign1.fxml")));
+		   page.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style/styles.css")).toExternalForm());
+				root.getChildren().addAll(NavigationBar.getNavigationBar(), page);
+				HBox.setHgrow(page, Priority.ALWAYS);
+			 
 
-			//StackPane page = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/SearchPage.fxml")));
-			//page.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style/SearchPage.css")).toExternalForm());
+					Palette DarkPalette = new Palette(
+							"DarkMode",
+							"#1e1f24",
+							"#23232f",
+							"#3f98fc"
+					);
 
-			HBox page = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/TicketPage.fxml")));
+					Palette LightPalette = new Palette(
+							"LightMode",
+							"#eef1fa",
+							"white",
+							"#3f98fc"
+					);
 
-			root.getChildren().addAll(NavigationBar.getNavigationBar(), page);
-			HBox.setHgrow(page, Priority.ALWAYS);
+					LightPalette.usePalette(scene);
+					//DarkPalette.usePalette(scene);
 
+					CSSFX.start();
+				primaryStage.setScene(scene);
+				primaryStage.setTitle("Flight Booking Application");
+				primaryStage.show();*/
 
-			Palette DarkPalette = new Palette(
-					"DarkMode",
-					"#1e1f24",
-					"#2d2d3c",
-					"#3f98fc"
-			);
-
-			Palette LightPalette = new Palette(
-					"LightMode",
-					"#eef1fa",
-					"white",
-					"#3f98fc"
-			);
-
-			//LightPalette.usePalette(scene);
-			DarkPalette.usePalette(scene);
-
-			CSSFX.start();
+			Parent root =  FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Signin.fxml")));
+			 
+			Scene scene= new Scene(root);
+			root.getStylesheets().add(getClass().getResource("/style/sign.css").toExternalForm());
+			String css= this.getClass().getResource("/style/application.css").toExternalForm();
+			scene.getStylesheets().add(css);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Flight Booking Application");
 			primaryStage.show();
+			
 		}
 		catch (Exception e) {
 			e.printStackTrace();
