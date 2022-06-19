c863203b6b9b61eee8e05283daea1027b43f5427
imane_quabbou <103893606+imane12j@users.noreply.github.com>
2022-06-19 17:08:26 +0100

    Add Payment Page

---
+++ FlightBookingApplication/src/main/java/controller/PaymentPageController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class PaymentPageController implements Initializable {
+
+    @FXML
+    private VBox creditCardList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for (int i = 0; i < 3; i++) {
+            try {
+                AnchorPane creditCard = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/CreditCard.fxml")));
+                creditCardList.getChildren().add(creditCard);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
