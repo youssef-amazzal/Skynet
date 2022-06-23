0041b6ebc9d9117688030db29182baf621e89d0f
Youssef Amazzal <79524624+youssef-amazzal@users.noreply.github.com>
2022-06-23 13:06:18 +0100

    Connect Pages Part-2

--- FlightBookingApplication/src/main/java/controller/AccountPageController.java
+++ FlightBookingApplication/src/main/java/controller/AccountPageController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -18,17 +22,26 @@ public class AccountPageController implements Initializable {
     private HBox parent;
 
     @FXML
-    private VBox page;
+    private VBox tabContent;
 
     @FXML
     private ToggleGroup tabs;
+
+    @FXML
+    private ToggleButton toggleCards;
+
+    @FXML
+    private ToggleButton togglePassword;
+
+    @FXML
+    private ToggleButton togglePersonal;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         parent.getStylesheets().add(getClass().getResource("/style/AccountPage.css").toExternalForm());
 
         try {
-            page.getChildren().add(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/accountPage/PersonalInformation.fxml"))));
+            tabContent.getChildren().add(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/accountPage/PersonalInformation.fxml"))));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,5 +52,32 @@ public class AccountPageController implements Initializable {
             if (newVal == null)
                 oldVal.setSelected(true);
         });
+    }
+
+    @FXML
+    void openCardsTab(ActionEvent event) {
+        loadTab("/view/accountPage/MyCards.fxml");
+    }
+
+    @FXML
+    void openPasswordTab(ActionEvent event) {
+        loadTab("/view/accountPage/Password-Security.fxml");
+    }
+
+    @FXML
+    void openPersonalTab(ActionEvent event) {
+        loadTab("/view/accountPage/PersonalInformation.fxml");
+    }
+
+    private void loadTab(String path) {
+        try {
+            Parent tab = FXMLLoader.load(getClass().getResource(path));
+            VBox.setVgrow(tab, Priority.ALWAYS);
+
+            tabContent.getChildren().clear();
+            tabContent.getChildren().add(tab);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
