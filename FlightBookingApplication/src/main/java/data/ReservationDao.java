d1527ce88cfd2aa1ceb0bfeccd25ec9d4457c62a
imane_quabbou <103893606+imane12j@users.noreply.github.com>
2022-07-02 23:42:05 +0100

    add reservation model and its Dao

---
+++ FlightBookingApplication/src/main/java/data/ReservationDao.java
@@ -0,0 +1,162 @@
+package data;
+
+import models.Reservation;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationDao implements Dao<Reservation> {
+    AccountDao accountDao = new AccountDao();
+    FlightDao flightDao = new FlightDao();
+    SeatDao seatDao = new SeatDao();
+
+
+    @Override
+    public void create(Reservation reservation) {
+        Connection conn = DataSource.getConnection();
+        String statement = "INSERT INTO reservations (id_flight, id_account, id_seat, nbr_luggages, weight) VALUES (?,?,?,?,?);";
+        try {
+            PreparedStatement query = conn.prepareStatement(statement);
+            query.setInt(1, reservation.getFlight().getId());
+            query.setInt(2, reservation.getAccount().getId());
+            query.setInt(3,reservation.getSeat().getId());
+            query.setInt(4,reservation.getNbrLuggages());
+            query.setDouble(5,reservation.getWeight());
+
+
+            query.executeUpdate();
+            query.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Reservation read(int id) {
+        Connection conn = DataSource.getConnection();
+        Reservation reservation = null;
+        String statement = "SELECT * FROM reservations WHERE id = ?;";
+        try {
+            PreparedStatement query = conn.prepareStatement(statement);
+            query.setInt(1, id);
+
+            ResultSet res = query.executeQuery();
+
+            if (res.next()) {
+                reservation = new Reservation();
+                reservation.setId(res.getInt("id"));
+                reservation.setFlight(flightDao.read(res.getInt("id_flight")));
+                reservation.setAccount(accountDao.read(res.getInt("id_account")));
+                reservation.setSeat(seatDao.read(res.getInt("id_seat")));
+                reservation.setNbrLuggages(res.getInt("nbr_luggages"));
+                reservation.setWeight(res.getInt("weight"));
+            }
+
+            query.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return reservation;
+    }
+
+
+    @Override
+    public List<Reservation> readAll() {
+        Connection conn = DataSource.getConnection();
+        List<Reservation> list = new ArrayList<Reservation>();
+
+        try {
+            PreparedStatement query = conn.prepareStatement("SELECT * FROM reservations;");
+            ResultSet res = query.executeQuery();
+            while (res.next()) {
+                Reservation reservation = new Reservation();
+                reservation.setId(res.getInt("id"));
+                reservation.setFlight(flightDao.read(res.getInt("id_flight")));
+                reservation.setAccount(accountDao.read(res.getInt("id_account")));
+                reservation.setSeat(seatDao.read(res.getInt("id_seat")));
+                reservation.setNbrLuggages(res.getInt("nbr_luggages"));
+                reservation.setWeight(res.getInt("weight"));
+
+                list.add(reservation);
+            }
+            return list;
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void update(int id, Reservation reservation) {
+        Connection conn = DataSource.getConnection();
+        Reservation original =  this.read(id);
+
+        String statement = "UPDATE reservations SET id_flight = ?, id_account = ?, id_seat = ?, nbr_luggages = ?, weight = ? WHERE id = ? ;";
+
+        try {
+            PreparedStatement query = conn.prepareStatement(statement);
+            query.setInt(6, id);
+
+            if (reservation.getFlight() != null) {
+                query.setInt(1, reservation.getFlight().getId());
+            }
+            else {
+                query.setInt(1, original.getFlight().getId());
+            }
+
+            if (reservation.getAccount() != null) {
+                query.setInt(2, reservation.getAccount().getId());
+            }
+            else {
+                query.setInt(2, original.getAccount().getId());
+            }
+
+            if (reservation.getSeat() != null) {
+                query.setInt(3, reservation.getSeat().getId());
+            }
+            else {
+                query.setInt(3, original.getSeat().getId());
+            }
+
+            if (reservation.getNbrLuggages() != -1) {
+                query.setInt(4, reservation.getNbrLuggages());
+            }
+            else {
+                query.setInt(4, original.getNbrLuggages());
+            }
+
+            if (reservation.getWeight() != -1) {
+                query.setDouble(5, reservation.getWeight());
+            }
+            else {
+                query.setDouble(5, original.getWeight());
+            }
+
+            query.executeUpdate();
+            query.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        Connection conn = DataSource.getConnection();
+        try {
+            PreparedStatement query = conn.prepareStatement("DELETE FROM reservations WHERE id = ? ;");
+            query.setInt(1, id);
+            query.executeUpdate();
+            query.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ FlightBookingApplication/src/main/java/models/Reservation.java
@@ -0,0 +1,63 @@
+package models;
+
+public class Reservation {
+    private int id;
+    private Flight flight;
+    private Account account;
+    private Seat seat;
+    private int nbrLuggages;
+    private double weight;
+
+    public Reservation() {
+        this.weight = -1;
+        this.nbrLuggages = -1;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    public int getNbrLuggages() {
+        return nbrLuggages;
+    }
+
+    public void setNbrLuggages(int nbrLuggages) {
+        this.nbrLuggages = nbrLuggages;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
