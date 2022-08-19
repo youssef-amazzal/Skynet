15bbb67ec30ff0b37f614ab17c5d717a1388edb3
Youssef Amazzal <79524624+youssef-amazzal@users.noreply.github.com>
2022-08-20 00:55:08 +0100

    prepare the necessary files for deploying the app

---
+++ FlightBookingApplication/build_app_windows.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+rem Set desired installer type: "app-image" "msi" "exe".
+set INSTALLER_TYPE=msi
+
+set MAIN_JAR=%PROJECT_Name%.jar
+
+rem Remove previously generated installers.
+IF EXIST target\installer rmdir /S /Q target\installer
+
+rem In the end we will find the package inside the target/installer directory.
+call "%JAVA_HOME%\bin\jpackage" ^
+  --type %INSTALLER_TYPE% ^
+  --dest target/installer ^
+  --input target/ ^
+  --name FlightBookingApp ^
+  --main-class %MAIN_CLASS% ^
+  --main-jar %MAIN_JAR% ^
+  --java-options -Xmx2048m ^
+  --icon src/main/resources/images/duke.ico ^
+  --app-version %APP_VERSION% ^
+  --vendor "ESTS" ^
+  --copyright "Copyright © 2022-23 ESTS Inc." ^
+  --win-dir-chooser ^
+  --win-shortcut ^
+  --win-per-user-install ^
+  --win-menu
---
+++ FlightBookingApplication/src/main/java/application/FlightBookingLauncher.java
@@ -0,0 +1,7 @@
+package application;
+
+public class FlightBookingLauncher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
