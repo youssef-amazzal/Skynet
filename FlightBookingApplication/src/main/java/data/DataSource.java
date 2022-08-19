795df74e68a3fa375a9a43542e74ae0a7eccfa45
Youssef Amazzal <79524624+youssef-amazzal@users.noreply.github.com>
2022-08-20 00:23:10 +0100

    change the database directory

--- FlightBookingApplication/src/main/java/data/DataSource.java
+++ FlightBookingApplication/src/main/java/data/DataSource.java
@@ -1,15 +1,32 @@
 package data;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.*;
+import java.util.Objects;
 
 public class DataSource {
     private static Connection connection;
 
     private DataSource() {
         try {
-            String path = "jdbc:sqlite:src/main/resources/dataBase/applicationDataBase.db";
-            connection  = DriverManager.getConnection(path);
-        } catch (SQLException e) {
+
+            Path dataBasePath = Path.of(System.getProperty("user.home"), "FlightBookingApp", "applicationDataBase.db");
+            String resourceName= "/dataBase/applicationDataBase.db";
+
+            if (!Files.exists(dataBasePath)) {
+                Files.createDirectories(dataBasePath.getParent());
+                try(InputStream inputStream = getClass().getResourceAsStream(resourceName)) {
+                    Objects.requireNonNull(inputStream, "Not found resource: " + resourceName);
+                    Files.copy(inputStream, dataBasePath);
+                }
+            }
+
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dataBasePath.toAbsolutePath());
+
+        } catch (SQLException | IOException e) {
             System.out.println(e.getMessage());
         }
     }
